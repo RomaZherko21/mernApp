af1d55d9bd76d6bd0ec98f19f079d04413f85d15
Roma Zherko <RomaZherko21@inbox.ru>
2021-01-02 15:59:11 +0300

    auth with JWT token

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import "materialize-css";
 import { useRoutes } from "./Routes";
 import { BrowserRouter } from "react-router-dom";
+import { useAuth } from "./hooks/auth.hook";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  const routes = useRoutes(false);
+  const { token, userId, logout, login } = useAuth();
+  const isAuthenticated = !!token;
+  const routes = useRoutes(isAuthenticated);
   return (
-    <BrowserRouter>
-      <div className="container">{routes}</div>;
-    </BrowserRouter>
+    <AuthContext.Provider value={{token, login, logout, isAuthenticated, userId}}>
+      <BrowserRouter>
+        <div className="container">{routes}</div>;
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,14 +1,40 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useHttp } from '../hooks/http.hook'
+import { useMessage } from '../hooks/message.hook';
+import {AuthContext} from '../context/AuthContext'
 
 export const AuthPage = () => {
 
+    const auth = useContext(AuthContext)
+    const message = useMessage()
+    const { loading, request, error, clearError } = useHttp();
     const [form, setForm] = useState({
         email: '',
         password: ''
     })
 
+    useEffect(() => {
+        message(error)
+        clearError()
+    }, [error, message, clearError])
+
+
     const changeHandler = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', { ...form })
+            message(data.message)
+        } catch (e) { }
+    }
+
+    const logInHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', { ...form })
+            auth.login(data.token, data.userId)
+        } catch (e) { }
     }
 
     return (
@@ -19,21 +45,25 @@ export const AuthPage = () => {
                     <div className="card-content white-text">
                         <span className="card-title">Authorization</span>
                         <div>
-                            <div class="input-field">
+                            <div className="input-field">
                                 <input placeholder="Email" id="email" type="text" name="email" className="yellow-input"
                                     onChange={changeHandler} />
-                                <label for="email">Email</label>
+                                <label htmlFor="email">Email</label>
                             </div>
-                            <div class="input-field">
+                            <div className="input-field">
                                 <input placeholder="Password" id="password" type="password" name="password" className="yellow-input"
                                     onChange={changeHandler} />
-                                <label for="password">Password</label>
+                                <label htmlFor="password">Password</label>
                             </div>
                         </div>
                     </div>
                     <div className="card-action">
-                        <button className="btn yellow darken-4" style={{ marginRight: 10 }}>Sign In</button>
-                        <button className="btn grey lighten-1 black-text">Sign Up</button>
+                        <button className="btn yellow darken-4" style={{ marginRight: 10 }}
+                            onClick={logInHandler}
+                            disabled={loading}>Sign In</button>
+                        <button className="btn grey lighten-1 black-text"
+                            onClick={registerHandler}
+                            disabled={loading}>Sign Up</button>
                     </div>
                 </div>
             </div>
